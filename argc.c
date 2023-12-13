d5f97f58dcc7ca37a3e92296612e3b3d28abccce
30nada <nada.saleh35735@gmail.com>
2023-12-12 22:45:41 -0800

    shell

---
+++ argc.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int arge, char **argv)
+{
+	printf("ARGC is %s\n", argv * );
+
+	int index = 0;
+
+	for (index = 0; index < arge; index++)
+	{
+	printf("ARGC[%d] = %s\n", index, argv[index]);
+	}
+	return (0);
+}
---
+++ getline.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+	char *store = NULL;
+	ssize_t size = 0;
+	ssize_t getRV;
+	char *temp;
+
+	getRV = getline(&store, &size, stdin);
+	if (getRV == -1)
+	{
+	free(store);
+	exit(0);
+	}
+	printf("before tokenizing\n");
+	printf("you entered %s", store);
+	printf("after tokenizing\n");
+	temp = strtok(store, "\n");
+	printf("you entered %s", temp);
+	free(store);
+	return(0);
+}
