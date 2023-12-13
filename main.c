64258922d3685148b405f80848f2f6c6d342e481
30nada <nada.saleh35735@gmail.com>
2023-12-12 21:32:59 -0800

    shell

---
+++ main.c
@@ -0,0 +1,25 @@
+#include "shell.h"
+
+/**
+ * main - simple shell main function
+ * @ac: count of argumnents
+ * @av: argumnents
+ * return: 0 always (success)
+ */
+int main(int ac, char **av);
+{
+	char *line = NULL;
+	/*char **command = NULL;*/
+	int status = 0;
+	(void) ac;
+	(void) av;
+	while (1)
+	{
+	line = read_line();
+	if (line == NULL) /* reache EOF Ctrl+D */
+	return (status);
+	printf("%s\n", line);
+	/*command = tokenizer(line);
+	 status = _execute(command, argv);*/
+	}
+}
